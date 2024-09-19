3454ce9f88f5545c9622aa7039c581d2227a6afd
saumanarsad <sauman.arshad@gmail.com>
2024-09-20 04:07:47 +0500

    Managers API

--- ragnar_app/views.py
+++ ragnar_app/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import *
+from .serializers import *
 
-# Create your views here.
+class ManagerViewSet(viewsets.ModelViewSet):
+    queryset = Manager.objects.all()
+    serializer_class = ManagerSerializer
