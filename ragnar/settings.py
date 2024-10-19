7355f5b604677b5f31465ce680bbd0ebe1595af1
saumanarsad <sauman.arshad@gmail.com>
2024-10-20 01:55:34 +0500

    Change base model

--- ragnar/settings.py
+++ ragnar/settings.py
@@ -110,7 +110,7 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
-
+AUTH_USER_MODEL = 'ragnar_app.user'
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
--- ragnar_app/models.py
+++ ragnar_app/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django_softdelete.models import SoftDeleteModel
+from django.contrib.auth.models import AbstractBaseUser
 
 class Manager(SoftDeleteModel,models.Model):
     first_name  = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email = models.EmailField(max_length=100,unique=True)
+    username = models.CharField(max_length=100,unique=True)
     password = models.CharField(max_length=100)
     cnic = models.CharField(max_length=15,unique=True)
     phone_number = models.CharField(max_length=15,unique=True)
@@ -14,14 +16,19 @@ class Manager(SoftDeleteModel,models.Model):
     class Meta:
         db_table = 'managers'
     
-class User(SoftDeleteModel,models.Model):
+class User(SoftDeleteModel,AbstractBaseUser):
     first_name  = models.CharField(max_length=100)
     last_name  = models.CharField(max_length=100)
+    username = models.CharField(max_length=100,unique=True)
     email = models.EmailField(max_length=100,unique=True)
     password = models.CharField(max_length=100)
     phone_number = models.CharField(max_length=15,unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
+
+    REQUIRED_FIELDS = []
+    USERNAME_FIELD = 'username'
+    
 
     class Meta:
         db_table = 'users'
